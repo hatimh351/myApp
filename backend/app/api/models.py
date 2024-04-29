24f932618d117d142aa0bf472ace64d4183e1508
Hatim Lahwaouir <hlahwaou@e1r6p14.1337.ma>
2024-04-29 10:52:00 +0100

    jwt authentication done

--- backend/app/api/models.py
+++ backend/app/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from datetime import date
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, **extra_fields):
+
+        if not username:
+            raise ValueError(("The username must be set"))
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class User(AbstractBaseUser):
+
+    username = models.CharField(max_length=40, unique=True)
+    date_joined = models.DateField(default=date.today)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+    def __str__(self) -> str:
+        return "'{}'".format(self.username, self.date_joined)
+
